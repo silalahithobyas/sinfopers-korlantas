be32e55fab60714fdec4903dd52b7fe158f19add
ihza.dafa <ihza.dafa@ui.ac.id>
2025-03-18 14:23:24 +0700

    Menambahkan fungsi create_chart di service.py untuk membuat struktur divisi baru

--- organizational_structure/service.py
+++ organizational_structure/service.py
@@ -9,6 +9,21 @@ from personnel_database.models.users import UserPersonil
 class OrganizationalStructureService(ABC):
 
     @classmethod
+    @transaction.atomic
+    def create_chart(cls, **data) :
+        nama = data.pop('nama_chart')
+        personnel_id = data.pop("personnel_id")
+        personnel = UserPersonil.objects.filter(id=personnel_id).first()
+
+        if(not personnel) :
+            raise BadRequestException(f"Personnel with id {personnel_id} not exists.")
+
+        nodes = Nodes.objects.create(personnel=personnel)
+        chart = Chart.objects.create(nama=nama, nodes=nodes)
+
+        return chart
+
+    @classmethod
     def update_nodes(cls, nodes_id, **data) :
         nodes = Nodes.objects.filter(id=nodes_id).first()
         personnel_id = data.pop("personnel_id")
