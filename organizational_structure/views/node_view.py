fbea999be0ffb143123b35338f27d8bc10c44e96
syafri.barry <syafri.barry@ui.ac.id>
2025-03-18 13:26:56 +0700

    feat(org-structure): add ChildNodeView for creating child nodes

--- organizational_structure/views/node_view.py
+++ organizational_structure/views/node_view.py
@@ -43,3 +43,24 @@ class NodeView(APIView) :
         except Exception as e :
             return Response(prepare_error_response(str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+class ChildNodeView(APIView) :
+    permission_classes = [IsAuthenticated,]
+
+    def __init__(self) :
+        self.node_serializer = CreateNodeSerializer
+        self.chart_serializer = ChartSerializer
+        self.service = OrganizationalStructureService
+
+    def post(self, request, chart_id) :
+        try :
+            serializer = self.node_serializer(data=request.data)
+            if(not serializer.is_valid()) :
+                return Response(serializer_error_response(serializer.errors), status.HTTP_400_BAD_REQUEST)
+            chart = self.service.create_child_node(chart_id, **serializer.data)
+            serializer_data = self.chart_serializer(chart).data
+            return Response(prepare_success_response(serializer_data), status.HTTP_201_CREATED)
+        except APIException as e :
+            return Response(prepare_error_response(str(e)), e.status_code)
+        except Exception as e :
+            return Response(prepare_error_response(str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR)
+
