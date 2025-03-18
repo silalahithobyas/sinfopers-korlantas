c0f393de6b11dbbc6fd8a86e37a8146097d8da31
ihza.dafa <ihza.dafa@ui.ac.id>
2025-03-18 14:27:55 +0700

    Membuat endpoint API delete di NodeView (node_view.py) untuk menghapus node dari struktur divisi

--- organizational_structure/views/node_view.py
+++ organizational_structure/views/node_view.py
@@ -9,26 +9,41 @@ from commons.applibs.response import prepare_error_response, prepare_success_res
 from organizational_structure.serializer import ChartSerializer, CreateNodeSerializer, UpdateNodeSerializer, DeleteNodeSerializer
 from organizational_structure.service import OrganizationalStructureService
 
-class NodeView(APIView) :
-    permission_classes = [IsAuthenticated,]
 
-    def __init__(self) :
+class NodeView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def __init__(self):
         self.node_serializer = UpdateNodeSerializer
         self.delete_node_serializer = DeleteNodeSerializer
         self.chart_serializer = ChartSerializer
         self.service = OrganizationalStructureService
 
-    def put(self, request, id) :
-        try :
+    def put(self, request, id):
+        try:
             serializer = self.node_serializer(data=request.data)
-            if(not serializer.is_valid()) :
+            if (not serializer.is_valid()):
                 return Response(serializer_error_response(serializer.errors), status.HTTP_400_BAD_REQUEST)
             self.service.update_nodes(id, **serializer.data)
             return Response(prepare_success_response(serializer.data), status.HTTP_201_CREATED)
-        except APIException as e :
+        except APIException as e:
             return Response(prepare_error_response(str(e)), e.status_code)
-        except Exception as e :
+        except Exception as e:
             return Response(prepare_error_response(str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, id):
+        try:
+            serializer = self.delete_node_serializer(data=request.data)
+            if (not serializer.is_valid()):
+                return Response(serializer_error_response(serializer.errors), status.HTTP_400_BAD_REQUEST)
+            chart = self.service.delete_node(serializer.data, id)
+            serializer_data = self.chart_serializer(chart).data
+            return Response(prepare_success_response(serializer_data), status.HTTP_201_CREATED)
+        except APIException as e:
+            return Response(prepare_error_response(str(e)), e.status_code)
+        except Exception as e:
+            return Response(prepare_error_response(str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class ChildNodeView(APIView) :
     permission_classes = [IsAuthenticated,]
