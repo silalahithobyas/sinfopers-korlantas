90c252198fcb721663c8dbf655a2d28ef4b0caeb
anandafa.syukur <anandafa.syukur@ui.ac.id>
2025-03-20 16:21:09 +0700

    .

--- authentication/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class IsAdminUser(permissions.BasePermission):
-    """
-    Custom permission to only allow admin users to access the view.
-    """
-    def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
