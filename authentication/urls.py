7718ad630d0e434be9c476fe1015a93e062af833
anandafa.syukur <anandafa.syukur@ui.ac.id>
2025-03-20 16:10:42 +0700

    Pendaftaran routing URL untuk login dan refresh token

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from authentication.views.user_view import UserLogin
+
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', UserLogin.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
