b661f16f33da4e6db579c7b83e5f78752aec4cd8
syafri.barry <syafri.barry@ui.ac.id>
2025-05-02 00:16:57 +0700

    make staffing status more user friendly in admin

--- staffing_status/admin.py
+++ staffing_status/admin.py
@@ -1,5 +1,70 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from django import forms
 from staffing_status.models import StaffingStatus
 
-admin.site.register(StaffingStatus)
+class StaffingStatusForm(forms.ModelForm):
+    class Meta:
+        model = StaffingStatus
+        fields = '__all__'
+        help_texts = {
+            'nama': 'Nama status kepegawaian (misalnya: "Status Pangkat Akpol", "Bidang Humas")',
+            'subsatker': 'Pilih sub-satuan kerja terkait',
+            'pangkat': 'Pilih pangkat terkait (bisa memilih lebih dari satu)',
+        }
+        widgets = {
+            'pangkat': forms.SelectMultiple(attrs={'size': '5'}),
+        }
+
+class StaffingStatusAdmin(admin.ModelAdmin):
+    form = StaffingStatusForm
+    list_display = ('nama', 'subsatker', 'get_pangkat', 'dsp', 'rill', 'status_kepegawaian')
+    search_fields = ('nama', 'subsatker__nama')
+    list_filter = ('subsatker', 'pangkat')
+    
+    fieldsets = (
+        ('Informasi Umum', {
+            'fields': ('nama', 'subsatker', 'pangkat'),
+            'description': 'Informasi dasar tentang status kepegawaian'
+        }),
+        ('Jumlah Personel', {
+            'fields': ('dsp', 'rill'),
+            'description': format_html(
+                '<div style="margin-bottom:10px;">'
+                '<p><strong>DSP (Daftar Susunan Personel)</strong>: Jumlah personel yang seharusnya/idealnya mengisi posisi ini.</p>'
+                '<p><strong>Riil</strong>: Jumlah personel aktual yang saat ini mengisi posisi ini.</p>'
+                '<p style="color:#666;"><i>Catatan: Nilai Riil akan bertambah otomatis saat personel baru ditambahkan dan berkurang '
+                'saat personel dihapus dari sistem dengan kombinasi pangkat dan subsatker yang sama.</i></p>'
+                '</div>'
+            )
+        }),
+    )
+    
+    def get_pangkat(self, obj):
+        return ", ".join([p.nama for p in obj.pangkat.all()])
+    get_pangkat.short_description = 'Pangkat'
+    
+    def status_kepegawaian(self, obj):
+        if obj.dsp > obj.rill:
+            return format_html('<span style="color:red;">Kekurangan {} personel</span>', obj.dsp - obj.rill)
+        elif obj.dsp < obj.rill:
+            return format_html('<span style="color:orange;">Kelebihan {} personel</span>', obj.rill - obj.dsp)
+        else:
+            return format_html('<span style="color:green;">Sudah terpenuhi</span>')
+    status_kepegawaian.short_description = 'Status'
+    
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if not change:  # Jika ini adalah data baru
+            self.message_user(
+                request, 
+                format_html(
+                    'Status kepegawaian berhasil dibuat.<br/>'
+                    '<strong>Catatan penting:</strong> Sekarang personel dengan pangkat {} dan subsatker {} '
+                    'dapat ditambahkan ke sistem.', 
+                    ', '.join([p.nama for p in obj.pangkat.all()]), 
+                    obj.subsatker.nama
+                )
+            )
+
+admin.site.register(StaffingStatus, StaffingStatusAdmin)
--- staffing_status/models.py
+++ staffing_status/models.py
@@ -7,8 +7,20 @@ class StaffingStatus(models.Model) :
     nama = models.CharField(max_length=120)
     subsatker = models.ForeignKey(SubSatKer, on_delete=models.CASCADE)
     pangkat = models.ManyToManyField(Pangkat)
-    dsp = models.IntegerField(default=0)
-    rill = models.IntegerField(default=0)
+    dsp = models.IntegerField(
+        default=0, 
+        verbose_name="DSP",
+        help_text="Daftar Susunan Personel - Jumlah personel yang seharusnya mengisi posisi ini"
+    )
+    rill = models.IntegerField(
+        default=0, 
+        verbose_name="Riil",
+        help_text="Jumlah personel aktual yang saat ini mengisi posisi ini"
+    )
 
     def __str__(self) :
         return f"{self.nama} - {self.subsatker.nama}"
+        
+    class Meta:
+        verbose_name = "Status Kepegawaian"
+        verbose_name_plural = "Status Kepegawaian"
