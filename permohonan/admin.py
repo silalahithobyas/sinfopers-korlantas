2eb32f9f5340ca3f4e3da8ee77a0437cbb763c8d
syafri.barry <syafri.barry@ui.ac.id>
2025-06-12 14:10:47 +0700

    live coding

--- permohonan/admin.py
+++ permohonan/admin.py
@@ -6,7 +6,6 @@ class PermohonanAdmin(admin.ModelAdmin):
     list_display = ('personel', 'jenis_permohonan', 'status', 'date_created')
     list_filter = ('jenis_permohonan', 'status')
     search_fields = ('personel__username', 'alasan')
-    readonly_fields = ('date_created', 'date_updated')
     fieldsets = (
         (None, {
             'fields': ('personel', 'jenis_permohonan', 'alasan', 'file_pendukung')
